b0e28712aa854907db5c990062d73876ec263626
Peter <peter@go-goody.com>
2020-12-07 21:14:35 -0800

    YelpCamp basic CRUD functionality

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,22 @@
+module.exports.places = [
+  'Flats',
+  'Village',
+  'Castle',
+  'Pond',
+  'Tumbling',
+  'Elk',
+  'Grizzly',
+  'Ocean',
+  'Sea',
+  'Sky'
+]
+
+module.exports.descriptors = [
+  'Forest',
+  'Ancient',
+  'Cascade',
+  'Roaring',
+  'Canyon',
+  'Group Camp',
+  'Horse Camp'
+]
